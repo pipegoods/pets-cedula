5d8d28d337f00fd90f27b48220224e021c09cee7
pipegoods <pipe.jaider@gmail.com>
2022-08-08 22:02:15 -0500

    💄  Update favicon and Title

--- components/Layouts/MainLayout.tsx
+++ components/Layouts/MainLayout.tsx
@@ -1,5 +1,6 @@
 import Header from '@components/Header';
 import { useSession } from 'next-auth/react';
+import Head from 'next/head';
 import type { MainLayoutProps } from 'types/MainLayout';
 
 const MainLayout = ({ children }: MainLayoutProps) => {
@@ -10,12 +11,19 @@ const MainLayout = ({ children }: MainLayoutProps) => {
   }
 
   return (
-    <div className="main-layout">
-      <Header />
-      <div className="hero min-h-screen bg-base-300">
-        <div className="hero-content text-center">{children}</div>
+    <>
+      <Head>
+        <title>Pets Cedula</title>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <div className="main-layout">
+        <Header />
+        <div className="hero min-h-screen bg-base-300">
+          <div className="hero-content text-center">{children}</div>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
